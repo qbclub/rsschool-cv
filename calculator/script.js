6b637066f447329f3d69dee9b6ebdab66eb589df
grachevrv <grachevrv@ya.ru>
2020-09-24 13:22:43 +0400

    feat: add tasks functions

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,21 @@
+const display = document.getElementById('display');
+
 const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operator');
-const clearBtns = document.querySelectorAll('.clear-btn');
-const decimalBtn = document.getElementById('decimal');
-const result = document.getElementById('result');
-const display = document.getElementById('display');
-let MemoryCurrentNumber = 0;
-let MemoryNewNumber = false;
-let MemoryPendingOperation = '';
 
+const squareRootBtn = document.getElementById('sqr')
+const exponentiationBtn = document.getElementById('exp')
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const decimalBtn = document.getElementById('decimal');
+const clearBtns = document.querySelectorAll('.clear-btn');
+const result = document.getElementById('result');
+
+let newNumber = false;
+let firstValue = 0;
+let action = ''
 
 for (var i = 0; i < numbers.length; i++) {
   var number = numbers[i];
@@ -32,10 +40,21 @@ for (var i = 0; i < clearBtns.length; i++) {
 
 decimalBtn.addEventListener('click', decimal);
 
+exponentiationBtn.addEventListener('click', exponentiation);
+squareRootBtn.addEventListener('click', squareRoot);
+plus.addEventListener("click", summation);
+minus.addEventListener("click", subtraction);
+multiply.addEventListener("click", multiplication);
+divide.addEventListener("click", division);
+result.addEventListener("click", equally);
+
+
+
+
 function numberPress(number) {
-  if (MemoryNewNumber) {
+  if (newNumber) {
     display.value = number;
-    MemoryNewNumber = false;
+    newNumber = false;
   } else {
     if (display.value === '0') {
       display.value = number;
@@ -45,51 +64,83 @@ function numberPress(number) {
   }
 }
 
-function operationPress(op) {
-  let localOperationMemory = display.value;
+function equally() {
 
-  if (MemoryNewNumber && MemoryPendingOperation !== '=') {
-    display.value = MemoryCurrentNumber;
-  } else {
-    MemoryNewNumber = true;
-    if (MemoryPendingOperation === '+') {
-      MemoryCurrentNumber += +localOperationMemory;
-    } else if (MemoryPendingOperation === '-') {
-      MemoryCurrentNumber -= +localOperationMemory;
-    } else if (MemoryPendingOperation === '*') {
-      MemoryCurrentNumber *= +localOperationMemory;
-    } else if (MemoryPendingOperation === '/') {
-      MemoryCurrentNumber /= +localOperationMemory;
-    } else {
-      MemoryCurrentNumber = +localOperationMemory;
-    }
-    display.value = MemoryCurrentNumber;
-    MemoryPendingOperation = op;
+  newNumber = true;
+  if (action == "exp") {
+    return display.value = Math.pow(firstValue, display.value)
+  }
+  if (action == "plus") {
+    return display.value = Number(firstValue) + Number(display.value)
+  }
+  if (action == "minus") {
+    return display.value = Number(firstValue) - Number(display.value)
+  }
+  if (action == "multiply") {
+    return display.value = Number(firstValue) * Number(display.value)
+  }
+  if (action == "divide") {
+    return display.value = Number(firstValue) / Number(display.value)
   }
 }
 
-function decimal(argument) {
-  let localDecimalMemory = display.value;
+function decimal() {
+  let localDecimal = display.value;
 
-  if (MemoryNewNumber) {
-    localDecimalMemory = '0.';
-    MemoryNewNumber = false;
+  if (newNumber) {
+    localDecimal = '0.';
+    newNumber = false;
   } else {
-    if (localDecimalMemory.indexOf('.') === -1) {
-      localDecimalMemory += '.';
+    if (localDecimal.indexOf('.') === -1) {
+      localDecimal += '.';
     }
   }
-  display.value = localDecimalMemory;
+  display.value = localDecimal;
+}
+
+function squareRoot() {
+  display.value >= 0 ? display.value = Math.sqrt(display.value) : display.value = "Нельзя"
+  newNumber = true;
+}
+
+function exponentiation() {
+  firstValue = display.value;
+  action = "exp";
+  newNumber = true;
+}
+
+function summation() {
+  firstValue = display.value;
+  action = "plus";
+  newNumber = true;
+}
+
+function subtraction() {
+  firstValue = display.value;
+  action = "minus";
+  newNumber = true;
+}
+
+function multiplication() {
+  firstValue = display.value;
+  action = "multiply";
+  newNumber = true;
+}
+
+function division() {
+  firstValue = display.value;
+  action = "divide";
+  newNumber = true;
 }
 
 function clear(id) {
   if (id === 'ce') {
     display.value = '0';
-    MemoryNewNumber = true;
+    newNumber = true;
   } else if (id === 'c') {
     display.value = '0';
-    MemoryNewNumber = true;
-    MemoryCurrentNumber = 0;
-    MemoryPendingOperation = '';
+    newNumber = true;
+    action = "";
+    firstValue = "";
   }
 }
